6b7b5e256a81dbdc21f311c80b112c6a4670a481
admin <admin>
2026-08-24 12:49:19 +0800

    Initial Commit

---
+++ TravPal/TravPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravPalApp.swift
+//  TravPal
+//
+//  Created by Revan Arturito on 24/08/26.
+//
+
+import SwiftUI
+
+@main
+struct TravPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
